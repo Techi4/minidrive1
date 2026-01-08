bcedb0998823b4638046a4315f8195bf176ef5a6
Naresh <gotojava5@gmail.com>
2026-01-09 03:19:50 +0530

    Add root endpoint to server response

--- server.js
+++ server.js
@@ -10,9 +10,14 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
+
 
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/files', require('./routes/fileRoutes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on ${PORT}`));
+
